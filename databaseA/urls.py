006423bc16a3776e7c0c589f6020ba481e527708
Ikram-2004 <149678364+Ikram-2004@users.noreply.github.com>
2026-05-19 18:08:21 +0530

    changestwo

--- databaseA/urls.py
+++ databaseA/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from inventory.views import home
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('inventory.urls')),
 ]
-
 
--- inventory/views.py
+++ inventory/views.py
@@ -28,20 +28,33 @@
 
 
 
+from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Stock
 
 API_KEY = "MY_SUPPLYCO_SECRET_KEY"
 
+
+# Homepage route
+def home(request):
+    return JsonResponse({
+        "message": "Supplyco Backend API Running",
+        "api_endpoint": "/api/updated-stock/"
+    })
+
+
+# API route
 @api_view(['GET'])
 def updated_stock(request):
 
+    # API KEY CHECK
     key = request.headers.get("X-API-KEY") or request.GET.get("api_key")
 
     if key != API_KEY:
         return Response({"error": "Unauthorized"}, status=401)
 
+    # Optional sync filter
     last_sync_time = request.GET.get('last_sync_time')
 
     if last_sync_time:
@@ -54,6 +67,7 @@ def updated_stock(request):
     else:
         stocks = Stock.objects.all().order_by('last_updated')
 
+    # Optimized response
     data = list(
         stocks.values(
             'store_id',
